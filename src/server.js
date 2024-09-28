8260c7c3fcbd9f0fd0f3f4cfd3e8e95ce47ce1af
sconsultancy <148803608+sconsultancy@users.noreply.github.com>
2024-09-28 23:53:03 +0530

    added nodemailer

--- src/server.js
+++ src/server.js
@@ -1,24 +1,48 @@
 const express = require("express");
-var cors = require('cors')
+var cors = require("cors");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
 
 const app = express();
+app.use(cors());
 
-app.use(cors())
+var jsonParser = bodyParser.json();
+
+// Nodemailer
+
 const PORT = process.env.PORT || 5000;
 
-app.get("/api",(req,res)=>{
-    res.send({message:"We Rock"})
-})
+app.post("/api/email", jsonParser, (req, res) => {
+  const auth = nodemailer.createTransport({
+    service: "gmail",
+    secure: true,
+    port: 465,
+    auth: {
+      user: "sconsultancygroup21@gmail.com",
+      pass: "dnhftjhavjkvmkzi",
+    },
+  });
 
-app.post("/api/email",(req,res)=>{
-    res.send({message:"Email"})
-})
+  const reciever = {
+    from: "sconsultancygroup21@gmail.com",
+    to: "ashutoshsharma9865@gmail.com",
+    subject: "Email home",
+    text: `${req.body.email}`,
+  };
 
-app.get("/",(req,res)=>{
-    res.send({message:"We Rock"})
-})
+  auth.sendMail(reciever, (error, emailResponse) => {
+    if (error) throw error;
+    console.log("success");
+    response.end();
+  });
 
+  res.send({ message: "Email sent" });
+});
 
-app.listen(PORT,()=>{
-    console.log("listening to port 5000");
+app.get("/", (req, res) => {
+  res.send({ message: "We Rock" });
+});
+
+app.listen(PORT, () => {
+  console.log("listening to port 5000");
 });
